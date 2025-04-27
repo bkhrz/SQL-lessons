bbb8e433b356b0135f85726a424c98b31ca08746
Behruz Qobilov <behruzqobilov26@gmail.com>
2025-04-28 01:58:57 +0500

    complete

---
+++ lesson-3/homework/task-3.sql
@@ -0,0 +1,35 @@
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Category VARCHAR(50),
+    Price DECIMAL(10,2),
+    Stock INT
+);
+
+INSERT INTO Products (ProductID, ProductName, Category, Price, Stock)
+VALUES 
+    (1, 'Laptop', 'Electronics', 1200, 15),
+    (2, 'Smartphone', 'Electronics', 800, 30),
+    (3, 'Desk Chair', 'Furniture', 150, 5),
+    (4, 'LED TV', 'Electronics', 1400, 8),
+    (5, 'Coffee Table', 'Furniture', 250, 0),
+    (6, 'Headphones', 'Accessories', 200, 25),
+    (7, 'Monitor', 'Electronics', 350, 12),
+    (8, 'Sofa', 'Furniture', 900, 2),
+    (9, 'Backpack', 'Accessories', 75, 50),
+    (10, 'Gaming Mouse', 'Accessories', 120, 20);
+
+
+select distinct 
+    Category,
+    ProductName,
+    Price,
+    IIF(Stock = 0, 'Out of stock',
+        IIF(Stock between 1 and 10, 'low stock', 'in stock')) as inventory_status
+from products
+where price = (
+    select max(price) from products as p2
+    where p2.Category = Products.Category
+)
+order by price DESC
+offset 5 rows;
